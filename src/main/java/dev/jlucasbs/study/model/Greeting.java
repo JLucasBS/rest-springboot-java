1eaea2ef28b68f48bbcdde46c7b8d6844d21bb95
João Lucas <jlucasbs1@gmail.com>
2025-09-30 12:00:12 -0300

    refactor: person with dependency injection

--- src/main/java/dev/jlucasbs/study/model/Greeting.java
+++
@@ -1,4 +0,0 @@
-package dev.jlucasbs.study.model;
-
-public record Greeting(long id, String content) {
-}
---
+++ src/main/java/dev/jlucasbs/study/services/PersonService.java
@@ -0,0 +1,26 @@
+package dev.jlucasbs.study.services;
+
+import dev.jlucasbs.study.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonService {
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public Person findByID(String id) {
+        logger.info("Finding person by ID: " + id);
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("João");
+        person.setLastName("Lucas");
+        person.setAddress("Leme - São Paulo - Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
+}
