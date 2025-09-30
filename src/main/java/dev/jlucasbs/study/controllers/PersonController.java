6af53e5ff55291a19da985180097a557b5f663f2
João Lucas <jlucasbs1@gmail.com>
2025-09-30 15:30:18 -0300

    feat: Spring Data JPA + MySQL

--- src/main/java/dev/jlucasbs/study/controllers/PersonController.java
+++ src/main/java/dev/jlucasbs/study/controllers/PersonController.java
@@ -4,6 +4,7 @@ import dev.jlucasbs.study.model.Person;
 import dev.jlucasbs.study.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,46 +16,29 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @RequestMapping(
-        value = "/{id}",
-        method = RequestMethod.GET,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public Person findByID(@PathVariable("id") String id) {
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findByID(@PathVariable("id") Long id) {
         return service.findByID(id);
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
     }
 
-    @RequestMapping(
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) {
         return service.create(person);
     }
 
-    @RequestMapping(
-            method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person) {
         return service.update(person);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.DELETE
-    )
-    public void update(@PathVariable("id") String id) {
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") Long id) {
         service.deleteByID(id);
+        return ResponseEntity.noContent().build();
     }
 }
