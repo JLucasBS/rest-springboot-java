cf679d52a0188ae0e9c72994ca7bd4acdca64f3e
João Lucas <jlucasbs1@gmail.com>
2025-09-30 11:33:49 -0300

    fix: imports

--- src/main/java/dev/jlucasbs/study/math/SimpleMath.java
+++ src/main/java/dev/jlucasbs/study/math/SimpleMath.java
@@ -1,11 +1,5 @@
 package dev.jlucasbs.study.math;
 
-import dev.jlucasbs.study.exception.UnsupportedMathOperationException;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import static dev.jlucasbs.study.request.converters.NumberConverter.convertToDouble;
-import static dev.jlucasbs.study.request.converters.NumberConverter.isNumeric;
-
 public class SimpleMath {
     public Double sum(Double numberOne, Double numberTwo) {
         return  numberOne + numberTwo;
